311fafa9afeecd736c7dcd5f6f26789c119b272d
caiobga99 <caiobga99@gmail.com>
2023-01-19 20:00:54 -0300

    React Native - finalizing the mobile layout

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -26,7 +26,7 @@ export function Checkbox({ title, checked = false, ...rest }: Props) {
       ) : (
         <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       )}
-      <Text className="text-white ml-3">{title}</Text>
+      <Text className="text-white ml-3 font-semibold">{title}</Text>
     </TouchableOpacity>
   );
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,14 +1,19 @@
-import { View, Text, ScrollView } from "react-native";
-import { HabitDay, DAY_SIZE } from "../components/HabitDay";
-import { Header } from "../components/Header";
+import { Text, View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-dates-from-year-start";
+
+import { Header } from "../components/Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSizes = 18 * 5;
-const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -30,7 +35,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({ length: amountOfDaysToFill }).map((_, i) => (
