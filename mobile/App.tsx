176bdfae98eaf029ef269d90c5e5f574df8da2c3
caiobga99 <caiobga99@gmail.com>
2023-01-19 19:26:49 -0300

    react-native - Adding habits record interface and routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,8 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
-import { Home } from "./src/screens/Home";
+import { Home } from "./src/screens/Home"; 
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
   }
   return (
     <>
-    <Home/>
+      <Routes />
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
