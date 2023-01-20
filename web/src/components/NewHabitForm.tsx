b6639e7da4c7425298281a1703a96db39eacd7d0
caiobga99 <caiobga99@gmail.com>
2023-01-20 10:50:30 -0300

    ReactJS - creation of checkboxes

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,4 +1,16 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
 export function NewHabitForm() {
   return (
     <form className="w-full flex flex-col mt-6">
@@ -11,14 +23,28 @@ export function NewHabitForm() {
         placeholder="ex.: Exercicios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3  bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
-<<<<<<< HEAD
-      /> 
-=======
       />
->>>>>>> 5c6185ebb5be5b6665c69d34531513e6f12cfef8
       <label className="font-semibold leading-tight mt-4">
         Qual a recorrencia?
       </label>
+      <div className="flex flex-col mt-3 gap-2">
+        {availableWeekDays.map((weekDay) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className="flex items-center gap-3 group"
+            >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className=" text-white leading-tight ">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
 
       <button
         type="submit"
