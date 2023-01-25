204d8faedf66a8bdaa82c0fd1140bb698ddb9d6c
caiobga99 <caiobga99@gmail.com>
2023-01-25 16:45:11 -0300

    ReactJS - adding transition and focus

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -8,7 +8,7 @@ export function ProgressBar(props: ProgressBarProps) {
         role="progressbar"
         aria-label="Progresso de hábitos completados nesse dia"
         arria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
+        className="h-3 rounded-xl bg-violet-600 transition-all"
         style={{
           width: `${props.progress}%`,
         }}
