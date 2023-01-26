6cb6451fee60950a985b1a0b9aa88e86c5ef6b1f
caiobga99 <caiobga99@gmail.com>
2023-01-26 18:16:53 -0300

    React native - Checking if the date being viewed has passed and alerting the user.

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -32,6 +32,7 @@ export function Habit() {
 
   const { date } = route.params as Params;
   const parsedDate = dayjs(date);
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date());
   const dayOfWeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
 
@@ -92,13 +93,18 @@ export function Habit() {
         </Text>
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View
+          className={clsx("mt-6", {
+            ["opacity-50"]: isDateInPast,
+          })}
+        >
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map((habit) => (
               <Checkbox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
                 onPress={() => handleToggleHabits(habit.id)}
               />
             ))
@@ -106,6 +112,11 @@ export function Habit() {
             <HabitsEmpty />
           )}
         </View>
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
